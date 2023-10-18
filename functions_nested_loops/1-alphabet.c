a6770802d048af057d391c8bffa6f2bd609eecb6
root <valibm>
2023-10-18 04:57:36 -0700

    Modified 1-alphabet.c and 1-main.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
 }
--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
-void print_alphabet(void)
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		_putchar(letter);
-	}
-
-	_putchar('\n');
+    print_alphabet();
+    return (0);
 }
