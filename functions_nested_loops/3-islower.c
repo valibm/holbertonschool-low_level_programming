364d037eab2e78a5c034b417358820a38286179f
root <valibm>
2023-10-18 23:05:08 -0700

    Added 3-islower.c and 3-main.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - It checks for lowercase character.
+ *
+ * Return: 1 if c is lowercase and 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
