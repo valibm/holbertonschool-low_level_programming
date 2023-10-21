192b98c42b2ef10cd98056cd4815796cbaea85c4
root <valibm>
2023-10-21 05:34:36 -0700

    Handled betty error in 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - prints all natural numbers from n to 98, followed by a new line
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line.
  * @n: starting number.
  * Return: Always 0.
  */
