8ec90afd5ab6fc73f1fa78ed280190aa4616822c
root <valibm>
2023-10-21 01:31:05 -0700

    Added 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,15 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = n % 10;
+	if (n < 0)
+	{
+		last_digit = -1 * (n % 10);
+	}
+	else
+	{
+		last_digit = n % 10;
+	}
 
-	_putchar(last_digit);
-	return (last_digit);
+	_putchar((last_digit % 10) + '0');
+	return (last_digit % 10);
 }
