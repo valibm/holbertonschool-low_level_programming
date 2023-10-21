301d41ad835f03c80488cc6a985ef2f82b8ea6c5
root <valibm>
2023-10-21 12:32:53 -0700

    Added 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 1)
+			printf("%d", i);
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			printf(" FizzBuzz");
+		else if ((i % 3) == 0)
+			printf(" Fizz");
+		else if ((i % 5) == 0)
+			printf(" Buzz");
+		else
+			printf(" %d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
