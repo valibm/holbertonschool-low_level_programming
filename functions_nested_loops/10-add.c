98c622f85e0407f5bf72e596a62227a565ac8dba
root <valibm>
2023-10-21 05:03:42 -0700

    Added 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add -  adds two integers and returns the result.
+ * @n1: integer type number.
+ * @n2: integer type number.
+ * Return: the addition of n1 and n2.
+ */
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
