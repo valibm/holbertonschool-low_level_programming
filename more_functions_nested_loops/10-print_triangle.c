b2d1b1e8ed104aa891d2fb26438bf12462b466fa
root <valibm>
2023-10-21 12:52:15 -0700

    Added 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,18 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 1; j <= size; j++)
-		{
-			if (j >= (size - i))
-				_putchar('#');
-			else
-				_putchar(' ');
-		}
+	if (size <= 0)
 		_putchar('\n');
-	}
+	else
+		for (i = 0; i < size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j >= (size - i))
+					_putchar('#');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
 }
