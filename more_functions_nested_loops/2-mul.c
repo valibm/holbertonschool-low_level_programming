1be0a61c8176a9e8a930c1b17dafb7e517eeca2b
root <valibm>
2023-10-21 09:35:51 -0700

    Added 2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - Is a function that multiplies two integers.
+ * @a: integer type number.
+ * @b: integer type number.
+ * Return: value of a * b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
