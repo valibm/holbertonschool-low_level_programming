4721bda80716667c139f5185efe03a544f5df0ba
root <valibm>
2023-10-23 12:40:56 -0700

    Handling the null termination problem

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 /**
- * _strcpy - a function that copies the string pointed to by src, including the
+0;136;0c * _strcpy - a function that copies the string pointed to by src, including the
  *  terminating null byte (\0), to the buffer pointed to by dest.
  * @dest: string that receives src's content.
  * @src: source string that's being copied.
@@ -10,10 +10,11 @@ char *_strcpy(char *dest, char *src)
 {
 	char *temp = dest;
 
-	while (*src)
+	while (*src != '\0')
 	{
 		*dest++ = *src++;
 	}
+	*dest = '\0';
 
 	return (temp);
 }
