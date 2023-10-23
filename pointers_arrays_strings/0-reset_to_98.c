3af10f5cd7c5c51a1b3fd33325b92ccb0e7ed1ef
root <valibm>
2023-10-23 03:44:04 -0700

    Added 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 /**
- * reset_to_98 - Is a function that takes a pointer to an int as parameter 
+ * reset_to_98 - Is a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98.
  *
  * Return: Always 0.
