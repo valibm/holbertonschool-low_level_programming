77f065c7afd44a720298b2e8d1840e8c84c093d9
root <valibm>
2023-10-26 05:12:35 -0700

    Added 6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include <stdbool.h>
+
+bool check_seperators(char c);
+
+/**
+ * cap_string - Is a function that capitalizes all words of a string.
+ * @str: string type parameter.
+ * Return: str with necessary capitalized words.
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (check_seperators(str[i]) &&
+		    str[i + 1] >= 97 && str[i + 1] <= 122)
+		{
+			str[i + 1] = str[i + 1] - 32;
+		}
+	}
+
+	return (str);
+}
+
+/**
+ * check_seperators - check if a is a eparators of words: space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ * @c: char type parameter.
+ * Return: 1 if seperator, 0 otherwise.
+ */
+bool check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+		{
+			return (true);
+		}
+	}
+
+	return (false);
+}
