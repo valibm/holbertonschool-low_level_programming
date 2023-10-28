24b254d7d9cc4c42ae0a5896d4b05e06e11bf567
root <valibm>
2023-10-28 05:43:18 -0700

    Added 3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+/**
+ * strspn - Is a function that gets the length of a prefix substring.
+ * @s: main (checked) string.
+ * @accept: prefix substring.
+ * Return: number of bytes in the initial segment of s which consist
+ * only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int length = 0, check;
+	char *start = accept;
+
+	while (*s)
+	{
+		check = 0;
+		while (*accept)
+		{
+			if (*accept == *s)
+			{
+				length++;
+				check = 1;
+				break;
+			}
+			accept++;
+		}
+		s++;
+		accept = start;
+		if (check == 0)
+			break;
+	}
+
+	return (length);
+}
