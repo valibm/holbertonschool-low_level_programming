74db75efeed280fa02725798ad735886266cf81e
root <valibm>
2023-10-29 03:34:47 -0700

    Added 100-rot13.c

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -7,17 +7,19 @@ char *rot13(char *s)
 {
 	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		if ((s[i] >= 'a' && s[i] <= 'z') ||
-		    (s[i] >= 'A' && s[i] <= 'Z'))
+		while ((s[i] >= 'a' && s[i] <= 'z') ||
+		       (s[i] >= 'A' && s[i] <= 'Z'))
 		{
 			if ((s[i] >= 'a' && s[i] <= 'm') ||
 			    (s[i] >= 'A' && s[i] <= 'M'))
 				s[i] += 13;
 			else
 				s[i] -= 13;
+			i++;
 		}
+		i++;
 	}
 
 	return (s);
