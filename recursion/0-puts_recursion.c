db3d3d635f5b7e6ec4c7698f7c26267c4c600d1d
root <valibm>
2023-10-30 00:36:07 -0700

    Handled betty style error

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Is a function that prints a string, followed by a new line.
+ * _puts_recursion - Is a function that prints a string,
+ * followed by a new line.
  * @s: printed string.
  * Return: Void.
  */
