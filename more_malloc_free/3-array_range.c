a0788bb5b6453b5c095e8453155265957e1bf486
root <valibm>
2023-11-11 03:43:08 -0800

    Handled betty errro in 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -16,16 +16,13 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(len * sizeof(int));
+	arr = malloc((len + 1) * sizeof(int));
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i <= len; i++)
 	{
-	        if (min == max)
-			break;
-		else
-			arr[i] = min++;
+		arr[i] = min++;
 	}
 
 	return (arr);
