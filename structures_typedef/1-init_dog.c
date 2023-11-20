0e788d1da02782de432438c0f5def3102891e4d6
root <valibm>
2023-11-20 07:52:01 -0800

    Adde 1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - It's a function that initialize a variable of type struct dog.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
