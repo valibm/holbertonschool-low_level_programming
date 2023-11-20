0a4b37f40958c228128747bb370a7fe607c064c2
root <valibm>
2023-11-20 07:28:37 -0800

    Added Readme file and the first task daog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOGSTRUCT
+#define DOGSTRUCT
+
+/**
+ * struct dog - It's a struct for a dog.
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif
