48d98cab1fb706752eb864e46614e040bf7b94b2
root <valibm>
2023-11-20 22:45:06 -0800

    Updated new_dog func

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * new_dog - It creates a new dog structure
@@ -11,6 +11,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *name_cpy, *owner_cpy;
+	int name_len = 0, owner_len = 0, i;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
@@ -18,6 +20,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
+
+	while(name[name_len])
+		name_len++;
+	while(owner[owner_len])
+		owner_len++;
+
+	name_cpy = malloc(name_len + 1);
+	if (name_cpy == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+		name_cpy[i] = name[i];
+
+	owner_cpy = malloc(owner_len + 1);
+	if (owner_cpy == NULL)
+		return (NULL);
+	for (i = 0; owner[i] != '\0'; i++)
+		owner_cpy[i] = owner[i];
 
 	new_dog->name = name;
 	new_dog->age = age;
