a216feda2241d22177349497b0584cb5ded4713d
root <valibm>
2023-12-10 08:34:29 -0800

    Added 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,38 +11,34 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp, *temp1, *node;
+	dlistint_t *node_current, *new_node;
+	unsigned int i = 0;
 
-	node = malloc(sizeof(dlistint_t));
-	if (node == NULL)
-		return (NULL);
-	node->prev = NULL;
-	node->n = n;
-	node->next = NULL;
-	if (!h || !(*h))
-		*h = node;
-	else
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	node_current = *h;
+
+	while (i < idx - 1)
 	{
-		temp = *h;
-		while (idx && temp->next)
-		{
-			temp = temp->next;
-			idx--;
-		}
-		if (idx)
-		{
-			temp->next = node;
-			node->prev = temp;
-		}
-		else
-		{
-			temp1 = temp->prev;
-			temp->prev = node;
-			temp1->next = node;
-			node->prev = temp1;
-			node->next = temp;
-		}
+		if (!node_current)
+			return (NULL);
+		node_current = node_current->next;
+		i++;
 	}
 
-	return (node);
+	if (!node_current->next)
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = node_current->next;
+	new_node->prev = node_current;
+	node_current->next->prev = new_node;
+	node_current->next = new_node;
+
+	return (new_node);
 }
