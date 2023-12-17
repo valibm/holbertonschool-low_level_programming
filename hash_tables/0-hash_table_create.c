0e8981d27d4747ef72e4a4a5a9d3f932c872e43c
root <valibm>
2023-12-17 04:04:22 -0800

    Added 0-hash_table_create.c

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: unsigned long integer
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table;
+
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_table_t *));
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
